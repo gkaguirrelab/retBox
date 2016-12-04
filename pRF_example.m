284cab3863391564be139417733cb10af8d9e06c
Andrew Bock <bock.andrew@gmail.com>
2016-12-04 16:14:09 -0500

    [ENH] pRF_example - updates to local example

--- pRF_example.m
+++ pRF_example.m
@@ -12,31 +12,36 @@ makePRFshellScripts(params)
 %% Run locally
 % Set inputs
 subjectName             = 'TOME_3001';
-sessionDir              = '/data/jag/TOME/TOME_3001/081916b';
-b                       = find_bold(sessionDir);
-runNum                  = 1;
-hemi                    = 'lh';
-matName                 = 'tfMRI_RETINO_PA_run01.mat';
-func                    = ['wdrf.tf.surf.' hemi];
-params.inVol            = fullfile(sessionDir,b{runNum},[func '.nii.gz']);
-params.stimFile         = fullfile(sessionDir,'Stimuli',matName);
-params.outDir           = fullfile(sessionDir,'pRFs');
-params.baseName         = sprintf([hemi '.run%02d'],runNum);
-% Calculate pRFs, save maps
-pRFs                    = makePRFmaps(params);
-% Plot the pRFs
-% Threshold by fit
-goodInd                 = pRFs.co>=sqrt(0.05);
-ecc                     = pRFs.ecc;
-pol                     = pRFs.pol;
-co                      = pRFs.co;
-sig                     = pRFs.sig;
-ecc(~goodInd)           = nan;
-pol(~goodInd)           = nan;
-co(~goodInd)            = nan;
-sig(~goodInd)           = nan;
-% Visualize maps
-surface_plot('ecc',ecc,subjectName);
-surface_plot('pol',pol,subjectName);
-surface_plot('co',co,subjectName);
-surface_plot('sig',sig,subjectName);
+params.sessionDir       = '/data/jag/TOME/TOME_3001/081916b';
+params.outDir           = fullfile(params.sessionDir,'pRFs');
+hemis                   = {'lh','rh'};
+% Get the retinotopy runs and stimulus files
+b                       = listdir(fullfile(params.sessionDir,'*RETINO*'),'dirs');
+stimFiles               = listdir(fullfile(params.sessionDir,'Stimuli','*RETINO*'),'files');
+% run `makePRFmaps
+for i = 1:length(b)
+    for hh = 1:length(hemis)
+        thisStim        = find(~cellfun('isempty',strfind(stimFiles,sprintf('run%02d',i))));
+        params.stimFile = fullfile(params.sessionDir,'Stimuli',stimFiles{thisStim});
+        params.inVol    = fullfile(params.sessionDir,b{i},['wdrf.tf.surf.' hemis{hh} '.nii.gz']);
+        params.baseName         = sprintf([hemis{hh} '.run%02d'],i);
+        % Calculate pRFs, save maps
+        pRFs                    = makePRFmaps(params);
+        % Plot the pRFs
+        % Threshold by fit
+        goodInd                 = pRFs.co>=sqrt(0.05);
+        ecc                     = pRFs.ecc;
+        pol                     = pRFs.pol;
+        co                      = pRFs.co;
+        sig                     = pRFs.sig;
+        ecc(~goodInd)           = nan;
+        pol(~goodInd)           = nan;
+        co(~goodInd)            = nan;
+        sig(~goodInd)           = nan;
+        % Visualize maps
+        surface_plot('ecc',ecc,subjectName);
+        surface_plot('pol',pol,subjectName);
+        surface_plot('co',co,subjectName);
+        surface_plot('sig',sig,subjectName);
+    end
+end
